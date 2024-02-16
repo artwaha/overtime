dab41c9655799b7e42cf3517a37795cebe7bf653
Abdulrahman Hemed Othman <abdulhemedi99@gmail.com>
2024-02-16 15:29:26 +0300

    Claim Item Response

--- src/main/java/orci/or/tz/overtime/dto/claims/ClaimItemResponseDto.java
+++ src/main/java/orci/or/tz/overtime/dto/claims/ClaimItemResponseDto.java
@@ -1,13 +1,17 @@
 package orci.or.tz.overtime.dto.claims;
 
 import lombok.Data;
+import orci.or.tz.overtime.enums.ClaimItemStatusEnum;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class ClaimItemResponseDto {
-    private Long claimItemId;
+    private Long id;
     private LocalDate claimDate;
     private String claimActivities;
+    private ClaimItemStatusEnum itemStatus;
     private ClaimResponseDto claim;
+    private LocalDateTime createdDate;
 }
